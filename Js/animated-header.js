794bba7aa2edd2cb0c154194b03a51fd11af0a93
Swarup2608 <swarupmantripragada@gmail.com>
2025-06-04 02:00:09 +0530

    Intial

---
+++ Js/animated-header.js
@@ -0,0 +1,37 @@
+const images = [];
+const images_main = [];
+for (let i = 1; i <= 20; i++) {
+    if (i <= 10) {
+        images.push(`scenery${i}.jpg`);
+        images_main.push(`Scenery/scenery${i}.jpg`);
+    }
+    if (i <= 7) {
+        images_main.push(`Macro/macro${i}.jpg`);
+    }
+    if (i <= 11) {
+        images_main.push(`Potraits/Potraits${i}.jpg`);
+    }
+    if (i <= 10) {
+        images_main.push(`Random/Random${i}.jpg`);
+    }
+}
+
+
+let index = 0;
+const backgroundDiv = document.getElementById('header');
+
+setInterval(() => {
+    index = (index + 1) % images.length;
+    backgroundDiv.style.background = `
+    linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.3)), 
+    url('../images/Scenery/${images[index]}') center/cover no-repeat fixed
+  `;
+}, 10000); // changes every 10 seconds
+
+const imagesMain = document.getElementById("images-gallery");
+for(var i = 1; i<images_main.length; i++){
+    const img = document.createElement('img');
+    img.src = "/images/"+images_main[i];
+    img.alt = images_main[i]+".jpg";
+    imagesMain.appendChild(img);
+}
